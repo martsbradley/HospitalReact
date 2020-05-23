36531156a9194873a014a7f3be6dc443b30c2a89
Martin Bradley <martsbradley@googlemail.com>
2020-05-23 13:38:13 +0100

    Buttons working nicely

--- src/patient/tablet/index.js
+++ src/patient/tablet/index.js
@@ -6,7 +6,6 @@ import PropTypes from 'prop-types';
 import StartDate from './start-date';
 import /*TabletWizardController,*/ {MyButtons,ButtonInfo} from './tablet-wizard-controller';
 import {tomorrowAsYYYYMMDD, todayAsYYYYMMDD} from '../../dateutils.js'
-import {matchPath} from 'react-router-dom'
  
 function NoMatch() {
   return <h1> No match</h1>;
@@ -19,7 +18,8 @@ export default function TabletWizard(props)
                                           endDate      : tomorrowAsYYYYMMDD(),
                                           selectedMedId: -1,
                                           validationMsg: '',
-                                          buttonArr: []});
+                                          page         :'select',
+                                          buttonArr    : []});
 
     const dateChanged = dateName => value => {
         setState(state => ({...state, 
@@ -28,27 +28,77 @@ export default function TabletWizard(props)
 
     useEffect(() => {
         console.log("TabletWizard rerender");
-        const buttonArr = makeButtonArray({...props, selectedMedId: state.selectedMedId});
+        const buttonArr = makeButtonArray({...props, medId: state.selectedMedId, page: state.page});
 
         setState(state => ({...state, buttonArr}));
     }
-    ,[]);
+    ,[state.selectedMedId, state.page]);
       
 
     function medicineSelectedFn(medicineSelected) {
 
         const {id, name } = medicineSelected;
-        //console.log(`TabletWizard id ${id} name ${name}`);
-        const buttonArr = makeButtonArray({...props, selectedMedId:id});
+
+        console.log(`medicineSelectedFn ${id}`);
+
         setState(state => ({...state,
                             selectedMedId: id,
-                            medicineName : name,
-                            buttonArr}));
+                            medicineName : name, }));
+        console.log(state);
+    }
+
+    function onNavigation(page) {
+        console.log(`onNavigation to ${page}`);
+        console.log(state);
+
+        setState(state => ({...state,
+                            page}));
+    }
+
+    const exitWizard = () =>{onNavigation('na');props.history.push("/patients/list");};
+    const goPage1    = () =>{onNavigation('select');props.history.push(`${props.match.path}/select`);};
+    const goPage2    = () =>{onNavigation('startDate');props.history.push(`${props.match.path}/startDate`);};
+    const goPage3    = () =>{onNavigation('endDate');props.history.push(`${props.match.path}/endDate`);};
+
+    const makeButtonArray = ({medId, page}) => {
+        console.log(`makeButtonArray ${page} ... ${medId}`);
+
+        const buttons = [];
+
+        if (page === 'select') {
+
+            let c = ButtonInfo('Back', false, exitWizard);
+            buttons.push(c);
+            let b = ButtonInfo('Next', false, goPage2);
+            buttons.push(b);
+
+            if (medId === -1) {
+                //console.log(`make ${b.label} false`);
+                b.isDisabled = true;
+            }
+        }
+        else if (page === 'startDate') {
+            let c = ButtonInfo('Back', false, goPage1);
+            buttons.push(c);
+            let b = ButtonInfo('Next', false, goPage3);
+            buttons.push(b);
+        }
+        else if (page === 'endDate') {
+            let c = ButtonInfo('Bakk', false, goPage2);
+            buttons.push(c);
+            let b = ButtonInfo('Next', false, goPage3);
+            buttons.push(b);
+        }
+        //console.log("returning");
+        //console.log(buttons);
+
+        return buttons;
     }
 
     return <ErrorBoundary>
         <>
-        name= {state.medicineName}
+            name= {state.medicineName}
+            name= {state.selectedMedId}
 
             <Switch>
                 <Route path={`${props.match.path}/select`}>
@@ -82,56 +132,12 @@ TabletWizard.propTypes = {
     history          : PropTypes.object,
 }
 
-const subPages = ['select','startDate','endDate'];
-const whichSubPage = (props, subPages) => 
-    subPages.find(subPage => 
-                         matchPath(props.location.pathname, 
-                                   { path: `${props.match.path}/${subPage}`}) !== null);
+
+ // import {matchPath} from 'react-router-dom'
+ // const subPages = ['select','startDate','endDate'];
+ // const whichSubPage = (props, subPages) => 
+ //     subPages.find(subPage => 
+ //                          matchPath(props.location.pathname, 
+ //                                    { path: `${props.match.path}/${subPage}`}) !== null);
 
 
-const makeButtonArray = (props) => {
-  const {selectedMedId} =  props;
-
-  const exitWizard = () => props.history.push("/patients/list");
-  const goPage1    = () => props.history.push(`${props.match.path}/select`);
-  const goPage2    = () => props.history.push(`${props.match.path}/startDate`);
-  const goPage3    = () => props.history.push(`${props.match.path}/endDate`);
-
-  const page = whichSubPage(props, subPages);
-
-  console.log(`Subpage is ${page} ${selectedMedId}`);
- /* console.log(`pathname = ${props.location.pathname}`);
-  * console.log(`path = ${props.match.path}`);
-  */
-
-    const buttons = [];
-
-    if (page === 'select') {
-
-        let c = ButtonInfo('Back', false, exitWizard);
-        buttons.push(c);
-        let b = ButtonInfo('Next', false, goPage2);
-        buttons.push(b);
-
-        if (selectedMedId === -1) {
-            //console.log(`make ${b.label} false`);
-            b.isDisabled = true;
-        }
-    }
-    else if (page === 'startDate') {
-        let c = ButtonInfo('Back', false, goPage1);
-        buttons.push(c);
-        let b = ButtonInfo('Next', false, goPage3);
-        buttons.push(b);
-    }
-    else if (page === 'endDate') {
-        let c = ButtonInfo('Bakk', false, goPage2);
-        buttons.push(c);
-        let b = ButtonInfo('Next', false, goPage3);
-        buttons.push(b);
-    }
-    console.log("returning");
-    console.log(buttons);
-
-    return buttons;
-}
