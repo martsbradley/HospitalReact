c70a64412abdfd453b582d51f2be6c73cc0e9c07
Martin Bradley <martsbradley@googlemail.com>
2020-07-10 21:28:58 +0100

    Fixed type issue

--- src/redux/reducers/validationReducer.js
+++ src/redux/reducers/validationReducer.js
@@ -3,9 +3,8 @@ import * as types from '../actions/actionTypes';
 export default function validationReducer(validationState , action){
     switch(action.type){
         case types.VALIDATION_CLEAR:
-              return [];
+              return {};
         case types.VALIDATION_SET:
-            //console.log("VALIDATION SET " + action.validation);
             return  action.validation;
         default:
             return validationState;
